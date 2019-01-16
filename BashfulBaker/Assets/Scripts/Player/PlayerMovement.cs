0698084e20cdbb7a2d01b1cb1eb0fca816a577d0
Joshua Navarro <janavarro95@gmail.com>
2019-01-16 12:20:21 -0800

    Added in character movement for WASD, Arrow Keys, and Controllers to all be the same.

---
+++ BashfulBaker/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A script which controls moving the player about the environment.
+/// </summary>
+public class PlayerMovement : MonoBehaviour {
+
+
+    [Tooltip("The default speed the player should move.")]
+    /// <summary>
+    /// The default speed the player should move.
+    /// </summary>
+    public float defaultSpeed = 1.0f;
+
+    [Tooltip("How much to dampen the player's speed. Bigger numbers means a slower player.")]
+    /// <summary>
+    /// How much to slow the player's speed down by since Unity calculates speed every update tick. ~60 times a second!
+    /// </summary>
+    public float movementDampening = 20.0f;
+
+    /// <summary>
+    /// How fast the player should move. This can be modified by anything we want later.
+    /// </summary>
+    public float MovementSpeed
+    {
+        get
+        {
+            return (defaultSpeed) / movementDampening;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.gameObject.transform.position += new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"),0)*MovementSpeed;
+	}
+}
