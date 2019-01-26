285015ec078d3f21cefdd1b6f35a24ed9d76b621
Brody <brparich@ucsc.edu>
2019-01-25 18:08:52 -0800

    Bones of animation
    :thumbsup:

---
+++ BashfulBaker/Assets/Scripts/Cutscene_Demo/Customer_Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Customer_Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpeakUp()
+    {
+        Debug.Log("gottem");
+    }
+}
