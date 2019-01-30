05d237f9f7e020a40f69c3c72aa6e6178c92956f
bsmithfagan <bsmithfa@ucsc.edu>
2019-01-30 14:59:59 -0800

    Added scene transitions and art assets

---
+++ BashfulBaker/Assets/Scripts/Kitchen/TrackPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackPlayer : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+            GetComponent<Renderer>().enabled = false;
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+            GetComponent<Renderer>().enabled = true;
+    }
+}
