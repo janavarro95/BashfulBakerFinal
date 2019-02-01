adeddc0e5b5f268a05449371cea58cbf94cdc13b
Brody <brparich@ucsc.edu>
2019-02-01 15:41:40 -0800

    Rolling Cookies and bug fixing
    just like the title

--- BashfulBaker/Assets/Scripts/Rolling_Cookies/SpinThumbstick.cs
+++ BashfulBaker/Assets/Scripts/Rolling_Cookies/SpinThumbstick.cs
@@ -1,18 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class SpinThumbstick : MonoBehaviour
+namespace Assets.Scripts.GameInput
 {
-    // Start is called before the first frame update
-    void Start()
+    public class SpinThumbstick : MonoBehaviour
     {
-        
-    }
+        private Vector2 startR, endR, startL, endL;
+        private float sumR, sumL;
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            startR = new Vector2(0, 0);
+            startL = new Vector2(0, 0);
+            endR = new Vector2(0, 0);
+            endL = new Vector2(0, 0);
+            sumR = 0;
+            sumL = 0;
+        }
 
+        // Update is called once per frame
+        void Update()
+        {
+            endR = new Vector2(GameInput.InputControls.RightJoystickHorizontal, GameInput.InputControls.RightJoystickVertical);
+            if (!startR.Equals(new Vector2(0, 0)) && !endR.Equals(new Vector2(0, 0)))
+            {
+                sumR += Vector2.Angle(startR, endR) > 45 ? 45 : Vector2.Angle(startR, endR);
+            }
+            startR = endR;
+
+            endL = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            if (!startL.Equals(new Vector2(0, 0)) && !endL.Equals(new Vector2(0, 0)))
+            {
+                sumL += Vector2.Angle(startL, endL) > 45 ? 45 : Vector2.Angle(startL, endL);
+            }
+            startL = endL;
+
+            if(sumR > 720 && sumL > 720)
+            {
+                Debug.Log("yay");
+            }
+        }
     }
 }
