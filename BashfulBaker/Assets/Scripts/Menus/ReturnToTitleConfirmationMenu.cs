3c5deff0fa09620d04286d0b3bc652ec7459ebf5
Joshua Navarro <janavarro95@gmail.com>
2019-02-12 17:25:41 -0800

    Created CS files for next step of menu work.

---
+++ BashfulBaker/Assets/Scripts/Menus/ReturnToTitleConfirmationMenu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Menus
+{
+    public class ReturnToTitleConfirmationMenu:Menu
+    {
+        public override void Start()
+        {
+           
+        }
+
+        public override void Update()
+        {
+            
+        }
+
+        public void yesButtonClick()
+        {
+
+        }
+
+        public void noButtonClick()
+        {
+
+        }
+
+    }
+}
