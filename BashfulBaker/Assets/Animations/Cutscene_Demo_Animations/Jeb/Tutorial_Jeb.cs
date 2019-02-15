8d8d40ff59553bca475c8415310a8d9bffc474d4
Brody <brparich@ucsc.edu>
2019-02-15 04:14:41 -0800

    First cutscene in
    It was a pain in the ass but it is 100% idiot proof

---
+++ BashfulBaker/Assets/Animations/Cutscene_Demo_Animations/Jeb/Tutorial_Jeb.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Assets.Scripts.GameInput;
+
+
+
+public class Tutorial_Jeb : MonoBehaviour
+{
+    public Animator jeb_animator;
+    private int ApressCount;
+    public Dialogue tutorial_lines_pt1;
+    public Dialogue tutorial_lines_pt2;
+    public Dialogue tutorial_lines_pt3;
+    private bool check_one;
+    private bool check_two;
+    private bool check_three;
+    public bool canCount;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // pressCount = -1;
+        //tutorial_lines.presentHeadshot = tutorial_lines.headshot1;
+
+        canCount = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (InputControls.APressed && canCount == true)
+        {
+            ApressCount++;
+            Debug.Log(ApressCount);
+        }
+        if (ApressCount >= 3 && check_three == true && jeb_animator.GetInteger("Movement_Phase") == 2)
+        {
+            //jeb_animator.SetInteger("Movement_Phase", 2);
+            End_Count();
+            Jeb_disappear();
+        }
+        if (ApressCount >= 5 && check_two == true && jeb_animator.GetInteger("Movement_Phase") == 1)
+        {
+            jeb_animator.SetInteger("Movement_Phase", 2);
+            End_Count();
+            check_three = true;
+        }
+
+        if (ApressCount >= 5 && check_one == true && jeb_animator.GetInteger("Movement_Phase") == 0)
+        {
+            jeb_animator.SetInteger("Movement_Phase", 1);
+            End_Count();
+            check_two = true;
+        }
+
+
+        
+
+        /*if (pressCount >= 10 && jeb_animator.GetInteger("Movement_Phase") == 2)
+        {
+           Bye();
+        }
+        if (pressCount >= 7 && jeb_animator.GetInteger("Movement_Phase") == 1)
+        {
+            jeb_animator.SetInteger("Movement_Phase", 2);
+        }
+        if (pressCount >= 5 && jeb_animator.GetInteger("Movement_Phase") == 0)
+        {
+            jeb_animator.SetInteger("Movement_Phase", 1);
+        }*/
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+      /*  if (InputControls.APressed)
+        {
+            pressCount++;
+            Debug.Log(pressCount);
+        }
+
+        if (InputControls.APressed && pressCount >= 6 && check_two == false)
+        {
+            Mission();
+            check_two = true;
+        }*/
+
+        if (InputControls.APressed && check_one == false)
+        {
+            Introduction();
+            //jeb_animator.SetInteger("Movement_Phase", 1);
+            //pressCount++;
+            check_one = true;
+            Begin_Count();
+        }
+
+
+
+
+    }
+    void Bye()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(tutorial_lines_pt3);
+    }
+    void Introduction()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(tutorial_lines_pt1);
+    }
+    void Mission()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(tutorial_lines_pt2);
+    }
+    void Jeb_disappear()
+    {
+        this.gameObject.SetActive(false);
+    }
+    void Begin_Count()
+    {
+        ApressCount = 0;
+        canCount = true;
+    }
+    void End_Count()
+    {
+        ApressCount = 0;
+        canCount = false;
+    }
+}
