b7725467106807460fc70b2016c65eb18679cb30
Brody <brparich@ucsc.edu>
2019-02-16 17:39:13 -0800

    Monologue and delivery speech done
    more dialogue and cutscene stuff

--- BashfulBaker/Assets/Animations/Cutscene_Demo_Animations/Jeb/Tutorial_Jeb.cs
+++ BashfulBaker/Assets/Animations/Cutscene_Demo_Animations/Jeb/Tutorial_Jeb.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Assets.Scripts.GameInput;
+using Assets.Scripts.GameInformation;
 
 
 
@@ -24,8 +25,6 @@ public class Tutorial_Jeb : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // pressCount = -1;
-        //tutorial_lines.presentHeadshot = tutorial_lines.headshot1;
         if(GameObject.Find("Player(Clone)").GetComponent<PlayerMovement>().currentStep != 0)
         {
             this.gameObject.SetActive(false);
@@ -45,7 +44,6 @@ public class Tutorial_Jeb : MonoBehaviour
         }
         if (ApressCount >= 3 && check_three == true && jeb_animator.GetInteger("Movement_Phase") == 2)
         {
-            //jeb_animator.SetInteger("Movement_Phase", 2);
             End_Count();
             Jeb_disappear();
         }
@@ -62,43 +60,15 @@ public class Tutorial_Jeb : MonoBehaviour
             End_Count();
             check_two = true;
         }
-
-
-        
-
-        /*if (pressCount >= 10 && jeb_animator.GetInteger("Movement_Phase") == 2)
-        {
-           Bye();
-        }
-        if (pressCount >= 7 && jeb_animator.GetInteger("Movement_Phase") == 1)
-        {
-            jeb_animator.SetInteger("Movement_Phase", 2);
-        }
-        if (pressCount >= 5 && jeb_animator.GetInteger("Movement_Phase") == 0)
-        {
-            jeb_animator.SetInteger("Movement_Phase", 1);
-        }*/
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
-      /*  if (InputControls.APressed)
-        {
-            pressCount++;
-            Debug.Log(pressCount);
-        }
 
-        if (InputControls.APressed && pressCount >= 6 && check_two == false)
-        {
-            Mission();
-            check_two = true;
-        }*/
 
         if (InputControls.APressed && check_one == false)
         {
             GameObject.Find("Player(Clone)").GetComponent<PlayerMovement>().defaultSpeed = 0;
             Introduction();
-            //jeb_animator.SetInteger("Movement_Phase", 1);
-            //pressCount++;
             check_one = true;
             Begin_Count();
         }
@@ -133,5 +103,10 @@ public class Tutorial_Jeb : MonoBehaviour
     {
         ApressCount = 0;
         canCount = false;
+    }
+    void turn_Dane()
+    {
+        Game.Player.facingDirection = Assets.Scripts.Enums.FacingDirection.Right;
+        //GameObject.Find("Player(Clone)").GetComponent<SpriteRenderer>().sprite = ;
     }
 }
