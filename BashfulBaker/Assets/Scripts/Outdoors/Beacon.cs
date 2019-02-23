259d5cb6f21109d43a16ed074fc70f0fc7c30503
Brody <brparich@ucsc.edu>
2019-02-22 17:43:43 -0800

    1st draft scent trail
    have particles and beacons

---
+++ BashfulBaker/Assets/Scripts/Outdoors/Beacon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beacon : MonoBehaviour
+{
+    public float Lifetime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Lifetime = 2.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Lifetime -= Time.deltaTime;
+        if (Lifetime < 0)
+        {
+            Debug.Log(this.transform.position.x);
+            Destroy(gameObject);
+        }
+    }
+}
