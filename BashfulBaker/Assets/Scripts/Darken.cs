9d886d7613ad414d1516743dadf50042cb879456
magicalporpoise <pdstanle@ucsc.edu>
2019-04-05 16:24:50 -0700

    There Goes the Neighborhood
    beginning to implement the final map for the true outside

---
+++ BashfulBaker/Assets/Scripts/Darken.cs
@@ -0,0 +1,26 @@
+﻿using Assets.Scripts.GameInformation;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Darken : MonoBehaviour
+{
+    public Material mat;
+    public float transparency, darkenSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transparency = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if(transparency < .9)
+            transparency += darkenSpeed;
+        */
+        transparency =1-(float)(Game.PhaseTimer.currentTime / Game.PhaseTimer.maxTime);
+        mat.SetFloat("_Transparency", transparency);
+    }
+}
---
+++ BashfulBaker/Assets/Scripts/PlayerArrowDirection.cs
@@ -0,0 +1,49 @@
+﻿using Assets.Scripts.GameInformation;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerArrowDirection : MonoBehaviour
+{
+
+    /// <summary>
+    /// The target object to look at.
+    /// </summary>
+    public Transform targetObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (targetObject != null)
+        {
+            lookAt();
+        }
+    }
+    /// <summary>
+    /// https://answers.unity.com/questions/585035/lookat-2d-equivalent-.html
+    /// </summary>
+    private void lookAt()
+    {
+        Vector3 diff = targetObject.position - transform.position; //Get the vector of difference to point the way,
+        diff.Normalize(); //Notmalize it.
+
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg; //Get the angle in degrees. Convert from radians to degrees
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90); //Create a new quaterion and make that the rotation.
+
+        this.gameObject.transform.parent.rotation= Quaternion.Euler(0f, 0f, rot_z - 270);
+
+    }
+
+    public void setTargetObject(GameObject target)
+    {
+        if (target != null)
+        {
+            targetObject = target.transform;
+        }
+    }
+}
