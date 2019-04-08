61e1928302bf6c36115664135441754fa68bee71
Brody <brparich@ucsc.edu>
2019-04-08 16:53:30 -0700

    file name change and rough draft of pour

---
+++ BashfulBaker/Assets/pouringWithController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Assets.Scripts.GameInformation;
+
+namespace Assets.Scripts.GameInput
+{
+    public class pouringWithController : MonoBehaviour
+    {
+        public GameObject bowl;
+      
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            if (bowl.transform.rotation.z >= -160)
+            {
+                bowl.transform.Rotate(new Vector3(0, 0, 1), (float)(-InputControls.RightTrigger * 3.6), Space.World);
+            }
+               //bowl.transform.Rotate(new Vector3(0, 0, 1), (float)(InputControls.LeftTrigger * 3.6), Space.World);
+            
+
+            if (bowl.transform.rotation.z < -12)
+            {
+                bowl.transform.Rotate(new Vector3(0, 0, 1), 1.8f, Space.World);
+            }
+        }
+    }
+
+}
