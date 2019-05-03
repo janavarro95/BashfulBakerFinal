3a4a48867e11bb6dc5e0a4c7bf5c5b4dcf5a40ee
Brody <brparich@ucsc.edu>
2019-05-03 16:47:14 -0700

    reee

--- BashfulBaker/Assets/Graphics/Characters/NPCS/Guards/Sully/SullyDay1.cs
+++ BashfulBaker/Assets/Graphics/Characters/NPCS/Guards/Sully/SullyDay1.cs
@@ -10,70 +10,61 @@ public class SullyDay1 : MonoBehaviour
 {
 
     public GameObject trigger;
-    private bool triggered;
-    private bool arrived;
-    private int ApressCount;
-    private bool canCount;
-    private int step;
+    public GameObject DiaBoxReference;
+    public Animator jeb_animator;
+    public bool isOpen;
+   public int step;
     public Dialogue Sully_Hey;
     public Dialogue Dane_Thanks;
     public Dialogue Sully_Bye;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        canCount = false;
+
         step = 0;
-        triggered = trigger.GetComponent<triggercheck>().beenHit;
+
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        isOpen = DiaBoxReference.GetComponent<DialogueManager>().IsDialogueUp;
 
-        if (InputControls.APressed && canCount == true)
-        {
-            ApressCount++;
-            //Debug.Log(ApressCount);
-        }
-
-        if (triggered && step == 0 && arrived == true)
-        {
-           
-            GameObject.Find("Player(Clone)").GetComponent<PlayerMovement>().defaultSpeed = 0;
-            killtrigger();
-            Begin_Count();
-            step++;
-        }
-        if (step ==1 && ApressCount == 4)
-        {
-            End_Count();
-
+        if (isOpen ==false && step > 0){
+            if (step == 3)
+            {
+                jeb_animator.SetInteger("Phase", 3);
+            }
+            if (step == 2)
+            {
+                Sully_Byebye();
+                step++;
+            }
+            if (step == 1)
+            {
+                Dane_Speaks();
+                step++;
+            }
+ 
 
         }
-    }
 
-    void endAmination()
-    {
-        triggered = false;
+
+        if (trigger.GetComponent<triggercheck>().beenHit == true && step == 0 && isOpen == false)
+        {
+           // step++;
+            jeb_animator.SetInteger("Phase", 1);
+        }
+
+
+
     }
-    void killtrigger()
+    private void endApproach()
     {
-        Destroy(GameObject.Find("triggercheck"));
+        jeb_animator.SetInteger("Phase", 2);
+        Debug.Log("set");
     }
-    void Begin_Count()
+    private void increasestep()
     {
-        ApressCount = 0;
-        canCount = true;
-    }
-    void End_Count()
-    {
-        ApressCount = 0;
-        canCount = false;
-    }
-    void release_Dane()
-    {
-        GameObject.Find("Player(Clone)").GetComponent<PlayerMovement>().defaultSpeed = 1.25f;
+        step++;
     }
     void Sully_Speaks()
     {
@@ -87,8 +78,72 @@ public class SullyDay1 : MonoBehaviour
     {
         FindObjectOfType<DialogueManager>().StartDialogue(Sully_Bye);
     }
-    void madeIt()
-    {
-        arrived = true;
-    }
+
+
+
+
+
+    // Start is called before the first frame update
+    /*  void Start()
+      {
+          canCount = false;
+          step = 0;
+          triggered = trigger.GetComponent<triggercheck>().beenHit;
+      }
+
+      // Update is called once per frame
+      void Update()
+      {
+
+          if (InputControls.APressed && canCount == true)
+          {
+              ApressCount++;
+              //Debug.Log(ApressCount);
+          }
+
+          if (triggered && step == 0 && arrived == true)
+          {
+
+              GameObject.Find("Player(Clone)").GetComponent<PlayerMovement>().defaultSpeed = 0;
+              killtrigger();
+              Begin_Count();
+              step++;
+          }
+          if (step ==1 && ApressCount == 4)
+          {
+              End_Count();
+
+
+          }
+      }
+
+      void endAmination()
+      {
+          triggered = false;
+      }
+      void killtrigger()
+      {
+          Destroy(GameObject.Find("triggercheck"));
+      }
+      void Begin_Count()
+      {
+          ApressCount = 0;
+          canCount = true;
+      }
+      void End_Count()
+      {
+          ApressCount = 0;
+          canCount = false;
+      }
+      void release_Dane()
+      {
+          GameObject.Find("Player(Clone)").GetComponent<PlayerMovement>().defaultSpeed = 1.25f;
+      }
+
+      void madeIt()
+      {
+          arrived = true;
+      }
+  }
+  */
 }
