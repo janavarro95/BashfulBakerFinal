87ff6bab6ab2eb0bff1ecf6c7a42c613bc2a6da3
JoshuaNavarro <janavarro95@gmail.com>
2019-05-20 16:14:26 -0700

    Fixed the neighboorhood.

--- BashfulBaker/Assets/Scripts/Menus/EndofDayMenu.cs
+++ BashfulBaker/Assets/Scripts/Menus/EndofDayMenu.cs
@@ -236,7 +236,7 @@ namespace Assets.Scripts.Menus
                     "UI",
                     "Menus",
                     "DailyRecap",
-                    "QuestButton_SylviaCC"
+                    "QuestMenu_button_SylviaCC"
                 }));
 
                 Sprite sprite = Game.ContentManager.loadSprite(texture, new Rect(new Rect(0, 0, 110, 38)), new Vector2(0.5f, 0.5f), 16);
