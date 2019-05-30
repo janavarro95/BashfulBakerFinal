58b143f78010e00ee3c33a215735a44de8245a15
magicalporpoise <pdstanle@ucsc.edu>
2019-05-30 15:01:02 -0700

    Dog Window
    - animated dog in the widow, will popup when player gets close

---
+++ BashfulBaker/Assets/WindowDogger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowDogger : MonoBehaviour
+{
+    // bools and motions
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = this.GetComponent<Animator>();
+        anim.enabled = false;
+    }
+
+    // when the player enters, play the dog pop up, then wag
+    // set speed to 1
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            anim.enabled = true;
+        }
+    }
+}
